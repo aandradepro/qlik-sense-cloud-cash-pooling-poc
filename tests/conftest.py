ead19f66967c060cd758a40777956fa4943c4c91
aandradepro <109821820+aandradepro@users.noreply.github.com>
2026-01-16 22:26:45 -0300

    Create fixtures for loading test data from CSVs
    Added pytest fixtures to load CSV data for tests.

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+import pandas as pd
+from pathlib import Path
+
+DATA_PATH = Path("../data/raw/")
+
+@pytest.fixture(scope="session")
+def holding():
+    return pd.read_csv(DATA_PATH / "holding.csv")
+
+@pytest.fixture(scope="session")
+def company():
+    return pd.read_csv(DATA_PATH / "company.csv")
+
+@pytest.fixture(scope="session")
+def country():
+    return pd.read_csv(DATA_PATH / "country.csv")
+
+@pytest.fixture(scope="session")
+def currency():
+    return pd.read_csv(DATA_PATH / "currency.csv")
+
+@pytest.fixture(scope="session")
+def cost_center():
+    return pd.read_csv(DATA_PATH / "cost_center.csv")
+
+@pytest.fixture(scope="session")
+def exchange_rate():
+    return pd.read_csv(
+        DATA_PATH / "exchange_rate.csv",
+        parse_dates=["calendar_date"]
+    )
+
+@pytest.fixture(scope="session")
+def cash_position():
+    return pd.read_csv(
+        DATA_PATH / "cash_position.csv",
+        parse_dates=["calendar_date"]
+    )
